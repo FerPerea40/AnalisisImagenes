4186c17f71c9184fe8bde960a78d6d68e4d1d0e1
FernandoPerea <ferperea0@gmail.com>
2021-04-22 09:02:46 -0500

    ultimo de imagenes

--- src/herramientas/HerramientasImagen.java
+++ src/herramientas/HerramientasImagen.java
@@ -158,9 +158,7 @@ public class HerramientasImagen {
     }
 
     public static Image ecualizacion(double[]histograma, Image imagenOriginal, int z){
-      // calculamos nxm
       int nxm = imagenOriginal.getHeight(null)*imagenOriginal.getWidth(null);
-      // declaramos el nuevo histograma
       int[] ecualizacion= new int[256];
       for(int g=0; g<z+1;g++){
           double tmp = 0;
@@ -169,14 +167,12 @@ public class HerramientasImagen {
             tmp/=nxm;
         ecualizacion[g]= (int)Math.round(tmp*z);
       }
-      // modificamos la imagen 
         BufferedImage bio = herramientas.HerramientasImagen.toBufferedImage(imagenOriginal);
         BufferedImage nueva = new BufferedImage(imagenOriginal.getWidth(null)
                                   , imagenOriginal.getHeight(null),BufferedImage.TYPE_INT_RGB);
         for(int y=0; y<imagenOriginal.getHeight(null);y++)
             for(int x=0; x<imagenOriginal.getWidth(null);x++){
-             // modificar el tono de la imagen en base al nuevo 
-             // histograma
+          
              Color color = new Color(bio.getRGB(x, y));
              int v = ecualizacion[color.getRed()];
              color = new Color(v, v, v);
